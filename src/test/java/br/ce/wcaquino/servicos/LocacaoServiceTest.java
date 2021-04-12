aaaaa15dcb2b2287edf30dc8f8f793d213e24127
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2021-04-12 15:29:53 -0300

    Test elegante

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -10,12 +10,20 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import java.util.Date;
 
+import static org.junit.Assert.assertThat;
+
 public class LocacaoServiceTest {
+
     @Rule
     public ErrorCollector error = new ErrorCollector();
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     @Test
     public void teste() {
         //cenario
@@ -44,4 +52,47 @@ public class LocacaoServiceTest {
 
 
     }
+
+    @Test(expected = Exception.class)
+    public void testLocacao_filmeSemEstoque() throws Exception{
+        //cenario
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario 1");
+        Filme filme = new Filme("Filme 2", 0, 4.0);
+
+        //acao
+        service.alugarFilme(usuario, filme);
+    }
+    @Test
+    public void testLocacao_filmeSemEstoque_2() {
+        //cenario
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario 1");
+        Filme filme = new Filme("Filme 2", 0, 4.0);
+
+        //acao
+        try {
+            service.alugarFilme(usuario, filme);
+            Assert.fail("Deveria ter lancado uma excecao");
+        } catch (Exception e) {
+            assertThat(e.getMessage(), CoreMatchers.is("Filme sem estoque"));
+        }
+    }
+
+
+    @Test
+    public void testLocacao_filmeSemEstoque_3() throws Exception {
+        //cenario
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario 1");
+        Filme filme = new Filme("Filme 2", 0, 4.0);
+
+        exception.expect(Exception.class);
+        exception.expectMessage("Filme sem estoque");
+
+        //acao
+        service.alugarFilme(usuario, filme);
+    }
+
+
 }
