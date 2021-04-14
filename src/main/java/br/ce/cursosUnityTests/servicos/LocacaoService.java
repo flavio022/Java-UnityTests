fdacdd7356ccf3bd8759e44cbfd383b81a4d7f8a
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2021-04-14 13:07:11 -0300

    Refatoracao

--- src/main/java/br/ce/cursosUnityTests/servicos/LocacaoService.java
+++ src/main/java/br/ce/cursosUnityTests/servicos/LocacaoService.java
@@ -40,23 +40,17 @@ public class LocacaoService {
 		locacao.setUsuario(usuario);
 		locacao.setDataLocacao(new Date());
 		Double valorTotal = 0d;
+
 		for(int i=0;i< filmes.size();i++){
 			Filme filme = filmes.get(i);
 			Double valorFilme = filme.getPrecoLocacao();
+			switch (i){
+				case 2: valorFilme = valorFilme*0.75; break;
+				case 3: valorFilme = valorFilme*0.5; break;
+				case 4: valorFilme = valorFilme*0.25; break;
+				case 5: valorFilme = valorFilme*0; break;
 
-			if(i==2){
-				valorFilme = valorFilme*0.75;
 			}
-			if(i==3){
-				valorFilme = valorFilme*0.5;
-			}
-			if(i==4){
-				valorFilme = valorFilme*0.25;
-			}
-			if(i==5){
-				valorFilme = valorFilme*0;
-			}
-
 			valorTotal += valorFilme;
 		}
 		locacao.setValor(valorTotal);
